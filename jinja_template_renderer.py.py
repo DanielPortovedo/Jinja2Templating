296283c497ad2d62e851158d25eaa8f55711bee0
daniel_portovedo <daniel.g.portovedo@gmail.com>
2024-03-03 18:01:42 +0100

    Added identities folder and values folder

--- jinja_template_renderer.py.py
+++ jinja_template_renderer.py.py
@@ -4,30 +4,35 @@ import json
 def main():
     renndered_content = None
 
+    # Read template file
     with open("values.j2", "r") as template_file:
         template_content = template_file.read()
 
+        # Load template file
         template = Template(template_content)
 
-        with open("values_bob.json", "r") as file:
+        # Read data from bob
+        with open("values/values_bob.json", "r") as file:
             data = file.read()
             
             data_contet = json.loads(data)
 
+            # Render bob data into template
             rendered_content = template.render(data_contet)
 
-        with open("identity_bob.txt", 'w') as output_file:
+        # Write rendered bob data into output file
+        with open("identities/identity_bob.txt", 'w') as output_file:
             output_file.write(rendered_content)
     
-
-        with open("values_alice.json", "r") as file:
+        # Repeat for Alice
+        with open("values/values_alice.json", "r") as file:
             data = file.read()
             
             data_contet = json.loads(data)
 
             rendered_content = template.render(data_contet)
 
-        with open("identity_alice.txt", 'w') as output_file:
+        with open("identities/identity_alice.txt", 'w') as output_file:
             output_file.write(rendered_content)
     
 if __name__ == "__main__":
